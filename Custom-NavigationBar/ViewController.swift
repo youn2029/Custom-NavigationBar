ce8bd815b4b5476213f928308011aab8d136bff7
youn2029 <cocoa8090@gmail.com>
2019-04-10 15:34:09 +0900

    내비게이션 아이템의 타이틀 설정 - UITextField

--- Custom-NavigationBar/ViewController.swift
+++ Custom-NavigationBar/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // 내비게이션 타이틀 초기화
-        self.initTitleImage()
+        self.initTitleInput()
         
         // 내비게이션 바의 색성
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
@@ -75,6 +75,26 @@ class ViewController: UIViewController {
         
         // 내비게이션 아이템의 타이틀 추가
         self.navigationItem.titleView = imageV
+        
+    }
+    
+    // 내비게이션 바의 타이틀 설정 - 텍스트 필드
+    func initTitleInput(){
+        
+        // 텍스트 필드
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 300, height: 35))
+        textField.backgroundColor = .white
+        textField.font = .systemFont(ofSize: 13)
+        textField.autocapitalizationType = .none    // 자동 대문자 변환 속성 x (UITextAutocapitalizationType)
+        textField.autocorrectionType = .no          // 자동 입력 기능 해제 (UITextAutocorrectionType)
+        textField.spellCheckingType = .no           // 스펠링 체크 기능 해제 (UITextSpellCheckingType)
+        textField.keyboardType = .URL               // URL 입력 전용 키보드 (UIKeyboardType)
+        textField.keyboardAppearance = .dark        // 키보드 색상 (UIKeyboardAppearance)
+        textField.layer.borderWidth = 0.3           // 테두리 경계선 두께
+        textField.layer.borderColor = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.0).cgColor
+        
+        // 내비게이션 아이템의 타이틀 추가
+        self.navigationItem.titleView = textField
         
     }
 
